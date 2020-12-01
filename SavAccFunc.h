ded48d511b5e29eb0262ac55897e9abe19072939
vbrfgd <vbrfgd@gmail.com>
2020-11-30 23:56:52 -0600

    Saving Account Update

---
+++ SavAccFunc.h
@@ -0,0 +1,47 @@
+/*
+- This file contains definitions of SavAcc class's functions
+*/
+
+#include "include.h"
+
+using namespace std;
+
+SavAcc::SavAcc(string newID, string newPassword, string newFname, string newLname, string newPhoneNum, double newInterestRate) : BankAcc(newID, newPassword, newFname, newLname, newInterestRate, 0.0)
+{
+    ID = newID.insert(0, "S");  //insert a C at the start of the acc number
+}
+
+void SavAcc::withdraw(double var)
+{
+    if (!isOnline())    // if the account is closed, message the user
+    {
+        cout << "\nInactive Account Cannot Be Withdrawed From.\n";
+        return;
+    }
+    if (var > balance)
+    {
+        cout << "\nWithdraw Ammount (" << var << ") Cannot Be Bigger Than The Account Balance (" << balance << ").\n";
+    }
+    else
+    {
+        balance -= var;
+    }
+    
+}
+void SavAcc::deposit(double var)
+{
+    if (!isOnline())    // if the account is closed, message the user
+    {
+        cout << "\nInactive Account Cannot Be Deposited Into.\n";
+        return;
+    }
+    else
+    {
+        balance += var;
+    }
+}
+
+void SavAcc::closeAcc()
+{
+    online = false;
+}
