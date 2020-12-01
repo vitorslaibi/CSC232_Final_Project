f4009bdc070cf76fe4c88153ef63d8972abfba54
vbrfgd <vbrfgd@gmail.com>
2020-11-30 23:45:50 -0600

    Include header file

---
+++ include.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include "BankAcc.h"
+#include "BankAccFunc.h"
+#include "SavAcc.h"
+#include "SavAccFunc.h"
+#include "ChkAcc.h"
+#include "ChkAccFunc.h"
+#include "mainFunc.h"
+
+#endif
