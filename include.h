9dbcc1fb81f38dbd7d4253fd1764fb2ea4c5fcbf
vitorslaibi <53208753+vitorslaibi@users.noreply.github.com>
2020-12-02 02:34:34 -0600

    Included new classes

--- include.h
+++ include.h
@@ -16,5 +16,9 @@
 #include "customerMenu.h"
 #include "officialMenu.h"
 #include "adminMenu.h"
+#include "SystemAdmin.h"
+#include "SystemAdminFunc.h"
+#include "BankOfficial.h"
+#include "BankOfficialFunc.h"
 
 #endif
