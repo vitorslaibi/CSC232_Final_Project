ee293c5dd9b29c03508d3d3b91eb07ff301c4ed3
vitorslaibi <53208753+vitorslaibi@users.noreply.github.com>
2020-12-04 14:56:05 -0600

    function prototypes updated

--- SystemAdmin.h
+++ SystemAdmin.h
@@ -21,15 +21,44 @@ class SystemAdmin{
 
     public:
     SystemAdmin(string newID, string newPassword, string newFname,string newLname);
+
     string getID();
     string getPassword();
     string getFname();
     string getLname();
-    int getOY();
-    int getOM();
-    int getOD();
-    void createOfficialAcc();
-    void changeLoginStatus(bool online);
-    void retrieveUserID();
-    void changePassword(string accNumber);
-};
+    int getCurrentY();
+    int getCurrentM();
+    int getCurrentD();
+
+    void createOfficialAcc(string newID, string newPassword, string newFname,string newLname);
+    void enableOfficialAcc(BankOfficial account);
+    void disableOfficialAcc(BankOfficial account);
+
+    void createUacc();
+    void createVacc();
+    void createXacc();
+    void createYacc();
+    void createZacc();
+
+    void modifyChkCharge(double newCharge);
+    void modifyChkInterest(double newInterest);
+    void modifySavCharge(double newCharge);
+    void modifySavInterest(double newInterest);
+    void modifyCDInterest(double newInterest);
+
+    void deleteChkAcc();
+    void deleteSavAcc();
+    void deleteCDacc();
+    void deleteUacc();
+    void deleteVacc();
+    void deleteXacc();
+    void deleteYacc();
+    void deleteZacc();
+
+    void changePasswordChk(ChkAcc account, string newpassword);
+    void changePasswordSav(SavAcc account, string newpassword);
+    void changePasswordCD(CD account, string newpassword);
+    string encryption(string password);
+    void updateFile(string oldPassword, string newPassword, string filename);
+    //save admin info in file
+    void saveData();
