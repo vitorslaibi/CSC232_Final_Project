69eb56af0dfed4d7040327aa1226119f0a2744db
vbrfgd <vbrfgd@gmail.com>
2020-12-04 13:11:13 -0600

    Debugging

--- BankAcc.h
+++ BankAcc.h
@@ -1,8 +1,8 @@
 /*
-- This file contains the BankAcc class, a prototype for the SavAcc and ChkAcc class.
+- This file contains the BankAcc class, a prototype for the other classes.
 - Its members are protected, which include an account's number, balance, interest rate, yearly service charge,
   online status, and last time (day and year) the balance was updated.
-- Its functions include a constructor to initialize the members, members getter and setter, functions to update
+- Its functions include a constructor to initialize the members, members getter and setter, time-related functions, functions to update
   the balance, and a function to close the account.
 */
 #include "include.h"    //include all files/libraries
@@ -13,30 +13,30 @@ class BankAcc
 {
     protected:
     string ID;  //account number
-    string password;
-    string fName;
-    string lName;
-    string phoneNum;
-    int openY;
-    int openM;
-    int openD;
-    int closeY;
-    int closeM;
-    int closeD;
+    string password;  //password
+    string fName;     //customer first name
+    string lName;     //customer last name
+    string phoneNum;  //customer phone number
+    int openY;        //the year the account get opened
+    int openM;        //the month the account get opened
+    int openD;        //the day the account get opened
+    int closeY;       //the year the account get closed
+    int closeM;       //the month the account get closed
+    int closeD;       //the day the account get closed
     double balance; //account balance
-    double safeLevel;
-    double penalty;
+    double safeLevel; //the safe benchmark for balance, if the balance goes below this amount, a penalty will be applied each month
+    double penalty;   //the penalty amount
     double interestRate;    //yearly interest rate
     double servCharge;      //yearly service charge
     bool online = true;     //online status; false means the account is closed
-    bool fixedInterest = false;
+    bool fixedInterest = false;   //flag indicates if the interest is fixed or variable for the account type
     int lastYearCounted;    //last year the balance was updated
-    int lastMonthCounted;
-    int lastDayCounted;     //last day in year the balance was updated
+    int lastMonthCounted;   //last month the balance was updated
+    int lastDayCounted;     //last day in month the balance was updated
 
     public:
-    int getYearDays(int currentYear);
-    int getMonthDays(int currentYear, int currentMonth);
+    int getYearDays(int currentYear);   //get number of days in a given year
+    int getMonthDays(int currentYear, int currentMonth);  //get number of day in a given month of a given year
     void updateDailyBalance(int year);  //update the balance after a day
     //getters and setters
     string getID();
@@ -73,7 +73,7 @@ class BankAcc
     void setLastDayCounted(int newLastDayCounted);      
     //end of getters and setters
     BankAcc(string newID, string newPassword, string newFname, string newLname, string newPhoneNum, double newInterestRate, double newServCharge, double newBalance);     //constructor
-    void updateOnlStat();
+    void updateOnlStat();   //set online stat for account
     virtual void deposit(double var) = 0;   //pure virtual function to deposit - to be overridden
     virtual void withdraw(double var) = 0;  //pure virtual function to withdraw - to be overridden
     void setLastTimeCounted();      //set the last time the balance was updated
@@ -82,15 +82,15 @@ class BankAcc
     int getCurrentD();
     int getCurrentH();
     int getCurrentMin();
-    void calcInt();                 //update the balance
+    void calcInt();                  //update the balance
     virtual void monthlyCharge();    //charge the service fee
-    void saveData();
-    void loadData();
-    void saveHistory(int year, int month, int day);
-    void saveTransactionHistory(char type, double amount);
+    void saveData();      //save customer data to a text file
+    void loadData();      //load customer data from a text file
+    void saveHistory(int year, int month, int day);   //save the day indicator to a text file
+    void saveTransactionHistory(char type, double amount);   //save the transaction history made
     void printAllHistory();
     void printLast7Days();
-    void getDayHistory(string text);
+    void getDayHistory(string text);  //get the history in a given day
     string encryption(string rawString);
     string decryption(string encryptedString);
     virtual void closeAcc() = 0;    //pure virtual function to close the acc - to be overridden
