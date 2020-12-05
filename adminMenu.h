5de028f99a188e09c9d712cccf1db70e01e7f135
Huy Huynh <huyhuynh.031201@gmail.com>
2020-12-04 19:45:38 -0600

    bug fixing

--- adminMenu.h
+++ adminMenu.h
@@ -15,26 +15,71 @@ void createAccMenu(vector<SystemAdmin> &adminList, int accNum, vector<ClassU> &u
             case 1:
             {
                 adminList[accNum].createUacc();
+                cout << "\nPlease Set The Safety Level\n";
+                double safeLev = getAmount();
+                cout << "\nPlease Enter the Penalty Amount\n";
+                double pen = getAmount();
+                for (int i = 0; i < uList.size(); i++)
+                {
+                    uList[i].setSafeLevel(safeLev);
+                    uList[i].setPenalty(pen);
+                }
                 break;
             }
             case 2:
             { 
                 adminList[accNum].createVacc();
+                cout << "\nPlease Set The Safety Level\n";
+                double safeLev = getAmount();
+                cout << "\nPlease Enter the Penalty Amount\n";
+                double pen = getAmount();
+                for (int i = 0; i < vList.size(); i++)
+                {
+                    vList[i].setSafeLevel(safeLev);
+                    vList[i].setPenalty(pen);
+                }
                 break;
             }
             case 3:
             { 
                 adminList[accNum].createXacc();
+                cout << "\nPlease Set The Safety Level\n";
+                double safeLev = getAmount();
+                cout << "\nPlease Enter the Penalty Amount\n";
+                double pen = getAmount();
+                for (int i = 0; i < xList.size(); i++)
+                {
+                    xList[i].setSafeLevel(safeLev);
+                    xList[i].setPenalty(pen);
+                }
                 break;
             }
             case 4:
             { 
                 adminList[accNum].createYacc();
+                cout << "\nPlease Set The Safety Level\n";
+                double safeLev = getAmount();
+                cout << "\nPlease Enter the Penalty Amount\n";
+                double pen = getAmount();
+                for (int i = 0; i < yList.size(); i++)
+                {
+                    yList[i].setSafeLevel(safeLev);
+                    yList[i].setPenalty(pen);
+                }
                 break;
             }
             case 5:
             { 
                 adminList[accNum].createZacc();
+                cout << "\nPlease Set The Safety Level\n";
+                double safeLev = getAmount();
+                cout << "\nPlease Enter the Penalty Amount\n";
+                double pen = getAmount();
+                for (int i = 0; i < zList.size(); i++)
+                {
+                    zList[i].setSafeLevel(safeLev);
+                    zList[i].setPenalty(pen);
+                }
                 break;
             }
             case 6:
@@ -91,6 +136,365 @@ void deleteAccMenu(vector<SystemAdmin> &adminList, int accNum, vector<ClassU> &u
                 break;
             }
             case 6:
+            {
+                done = true;
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+        cout << endl;
+    }
+}
+
+void modSav(vector<SavAcc> &savList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < savList.size(); i++)
+                {
+                    savList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < savList.size(); i++)
+                {
+                    savList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < savList.size(); i++)
+                {
+                    savList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modU(vector<ClassU> &uList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < uList.size(); i++)
+                {
+                    uList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < uList.size(); i++)
+                {
+                    uList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < uList.size(); i++)
+                {
+                    uList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modV(vector<ClassV> &vList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < vList.size(); i++)
+                {
+                    vList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < vList.size(); i++)
+                {
+                    vList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < vList.size(); i++)
+                {
+                    vList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modX(vector<ClassX> &xList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < xList.size(); i++)
+                {
+                    xList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < xList.size(); i++)
+                {
+                    xList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < xList.size(); i++)
+                {
+                    xList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modY(vector<ClassY> &yList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < yList.size(); i++)
+                {
+                    yList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < yList.size(); i++)
+                {
+                    yList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < yList.size(); i++)
+                {
+                    yList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modZ(vector<ClassZ> &zList, vector<SystemAdmin> &adminList, int accNum)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Interest Rate\n";
+        cout << "[2] Modify Penalty\n";
+        cout << "[3] Modify Safety Level\n";
+        cout << "[4] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                cout << "\nPlease Enter new Interest rate\n";
+                double rate = getRate();
+                for (int i = 0; i < zList.size(); i++)
+                {
+                    zList[i].setInterestRate(rate);
+                }
+            }
+            case 2:
+            {
+                cout << "\nPlease Enter new Penalty Amount\n";
+                double amount = getAmount();
+                for (int i = 0; i < zList.size(); i++)
+                {
+                    zList[i].setPenalty(amount);
+                }
+            }
+            case 3:
+            {
+                cout << "\nPlease Enter new Safety Level\n";
+                double safeLevel = getRate();
+                for (int i = 0; i < zList.size(); i++)
+                {
+                    zList[i].setSafeLevel(safeLevel);
+                }
+            }
+            case 4: 
+            {
+                done = true;
+                break;
+            }
+            default:
+                break;
+        }       
+    }
+}
+
+void modAccMenu(vector<SavAcc> &savList, vector<SystemAdmin> &adminList, int accNum, vector<ClassU> &uList, vector<ClassV> &vList, vector<ClassX> &xList, vector<ClassY> &yList, vector<ClassZ> &zList)
+{
+    bool done = false;
+    while (!done)
+    {
+        cout << "[1] Modify Saving Account\n";
+        cout << "[2] Modify U Account\n";
+        cout << "[3] Modify V Account\n";
+        cout << "[4] Modify X Account\n";
+        cout << "[5] Modify Y Account\n";
+        cout << "[6] Modify Z Account\n";
+        cout << "[7] Go Back\n";
+        int input = stoi(getInput());
+        switch (input)
+        {
+            case 1:
+            {
+                modSav(savList, adminList, accNum);
+                break;
+            }
+            case 2:
+            { 
+                modU(uList, adminList, accNum);
+                break;
+            }
+            case 3:
+            { 
+                modV(vList, adminList, accNum);
+                break;
+            }
+            case 4:
+            { 
+                modX(xList, adminList, accNum);
+                break;
+            }
+            case 5:
+            { 
+                modY(yList, adminList, accNum);
+                break;
+            }
+            case 6:
+            {
+                modZ(zList, adminList, accNum);
+                break;
+            }
+            case 7:
             {
                 done = true;
                 break;
@@ -279,7 +683,7 @@ void adminMenu(vector<ChkAcc> &chkList, vector<SavAcc> &savList, vector<CD> &cdL
             }
             case 6:
             { 
-                //modBankType();
+                modAccMenu(savList, adminList, accNum, uList, vList, xList, yList, zList);
                 cout << "\nBank Account Type Modified\n";
                 break;
             }
